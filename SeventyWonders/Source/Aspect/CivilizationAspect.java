6c0aae7bc66c7dc4066bd8d55fcad73043685fe5
William Fu <williamfu4github@gmail.com>
2019-10-28 03:35:06 -0400

    add method getBenefits to class CivilizationAspect

--- SeventyWonders/Source/Aspect/CivilizationAspect.java
+++ SeventyWonders/Source/Aspect/CivilizationAspect.java
@@ -1,10 +1,14 @@
 package Aspect;
 
+import Benefit.CivilizationBenefit;
 import lombok.EqualsAndHashCode;
+import java.util.Set;
 
 @EqualsAndHashCode
 public abstract class CivilizationAspect {
 
     protected CivilizationAspect() {
     }
+
+    public abstract Set<CivilizationBenefit> getBenefits();
 }
