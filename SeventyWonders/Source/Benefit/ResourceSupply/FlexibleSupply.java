8329a0bb55d987e5ee7aa6199b1c5da000a5ea17
William Fu <williamfu4github@gmail.com>
2020-02-06 17:36:49 -0500

    add field possibleItems to class FlexibleSupply

--- SeventyWonders/Source/Benefit/ResourceSupply/FlexibleSupply.java
+++ SeventyWonders/Source/Benefit/ResourceSupply/FlexibleSupply.java
@@ -1,10 +1,20 @@
 package Benefit.ResourceSupply;
 
+import Resource.ResourceType;
+import com.google.common.base.Preconditions;
 import lombok.EqualsAndHashCode;
+import java.util.Set;
+import java.util.stream.Stream;
 
 @EqualsAndHashCode
 public final class FlexibleSupply extends ResourceSupply {
 
-    public FlexibleSupply() {
+    @EqualsAndHashCode.Include
+    private Set<ResourceType> possibleItems;
+
+    public FlexibleSupply(ResourceType option1, ResourceType option2, ResourceType... otherOptions) {
+        this.possibleItems = Stream.concat(Stream.of(option1, option2), Stream.of(otherOptions))
+                .toSet();
+        Preconditions.checkArgument(this.possibleItems.size() == otherOptions.length + 2, "possibleItems cannot have duplicates");
     }
 }
--- SeventyWonders/Test/UnitTest/Benefit/ResourceSupply/FlexibleSupplyTest.java
+++ SeventyWonders/Test/UnitTest/Benefit/ResourceSupply/FlexibleSupplyTest.java
@@ -1,30 +1,57 @@
 package UnitTest.Benefit.ResourceSupply;
 
 import Benefit.ResourceSupply.FlexibleSupply;
+import Resource.ResourceType;
 import TesterBase.UnitTest.SingleClassUnitTest;
+import manifold.ext.api.Jailbreak;
 import org.junit.jupiter.api.Test;
+import java.util.Set;
 import static Utility.TestInstanceFactory.ResourceSupplyTest;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public final class FlexibleSupplyTest implements SingleClassUnitTest<FlexibleSupply> {
 
     @Override
-    public void clearInstance(FlexibleSupply instance) {
+    public void clearInstance(@Jailbreak FlexibleSupply instance) {
         ResourceSupplyTest.clearInstance(instance);
+        instance.possibleItems.clear();
     }
 
     @Override
     public FlexibleSupply createRealInstance() {
-        return new FlexibleSupply();
+        return new FlexibleSupply(ResourceType.ORE, ResourceType.WOOD);
     }
 
     @Test
     public void test_equals_positive() {
-        assertEquals(this.createInstance(), this.createInstance());
+        @Jailbreak FlexibleSupply supply1 = this.createInstance();
+        supply1.possibleItems = Set.of(ResourceType.ORE, ResourceType.WOOD);
+        @Jailbreak FlexibleSupply supply2 = this.createInstance();
+        supply2.possibleItems = Set.of(ResourceType.ORE, ResourceType.WOOD);
+        assertEquals(supply1, supply2);
     }
 
     @Test
     public void test_equals_negative() {
-        // cannot be not equal
+        @Jailbreak FlexibleSupply supply1 = this.createInstance();
+        supply1.possibleItems = Set.of(ResourceType.ORE, ResourceType.WOOD);
+        @Jailbreak FlexibleSupply supply2 = this.createInstance();
+        supply2.possibleItems = Set.of(ResourceType.ORE, ResourceType.CLAY);
+        assertNotEquals(supply1, supply2);
+    }
+
+    @Test
+    public void test_constructor_invalidArgument_duplicateSupplyOption() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new FlexibleSupply(ResourceType.ORE, ResourceType.WOOD, ResourceType.ORE);
+        });
+    }
+
+    @Test
+    public void test_constructor_happyPath() {
+        @Jailbreak FlexibleSupply supply = new FlexibleSupply(ResourceType.ORE, ResourceType.WOOD, ResourceType.CLAY);
+        assertEquals(Set.of(ResourceType.ORE, ResourceType.WOOD, ResourceType.CLAY), supply.possibleItems);
     }
 }
