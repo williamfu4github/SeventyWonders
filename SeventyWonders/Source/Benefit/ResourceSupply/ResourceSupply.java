635a8440302202583606c0f9614f179dab149d5c
William Fu <williamfu4github@gmail.com>
2020-02-06 17:54:09 -0500

    add method markResourceContribution to class ResourceSupply

--- SeventyWonders/Source/Benefit/ResourceSupply/ResourceSupply.java
+++ SeventyWonders/Source/Benefit/ResourceSupply/ResourceSupply.java
@@ -1,7 +1,9 @@
 package Benefit.ResourceSupply;
 
 import Benefit.CivilizationBenefit;
+import Resource.ResourceProfile;
 import Resource.ResourceType;
+import com.google.common.base.Preconditions;
 import lombok.EqualsAndHashCode;
 import java.util.Set;
 
@@ -9,6 +11,15 @@ import java.util.Set;
 public abstract class ResourceSupply extends CivilizationBenefit {
 
     protected ResourceSupply() {
+    }
+
+    public Set<ResourceProfile> markResourceContribution(Set<ResourceProfile> possibleProfiles) {
+        Preconditions.checkArgument(possibleProfiles.size() > 0);
+        Set<ResourceType> possibleSupplies = this.getPossibleSupplies();
+        return possibleSupplies.isEmpty() ? possibleProfiles : possibleProfiles.stream()
+                .flatMap(profile -> possibleSupplies.stream()
+                        .map(type -> ResourceProfile.copyOf(profile).increaseResourceAmount(type)))
+                .toSet();
     }
 
     protected abstract Set<ResourceType> getPossibleSupplies();
--- SeventyWonders/Test/UnitTest/Benefit/ResourceSupply/ResourceSupplyTest.java
+++ SeventyWonders/Test/UnitTest/Benefit/ResourceSupply/ResourceSupplyTest.java
@@ -1,11 +1,19 @@
 package UnitTest.Benefit.ResourceSupply;
 
 import Benefit.ResourceSupply.ResourceSupply;
+import Resource.ResourceProfile;
+import Resource.ResourceType;
 import TesterBase.UnitTest.SingleClassUnitTest;
+import manifold.ext.api.Jailbreak;
 import org.junit.jupiter.api.Test;
+import java.util.Collections;
+import java.util.Set;
 import static Utility.TestInstanceFactory.CivilizationBenefitTest;
 import static Utility.TestInstanceFactory.DedicatedSupplyTest;
+import static Utility.TestInstanceFactory.ResourceProfileTest;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.powermock.api.mockito.PowerMockito.when;
 
 public final class ResourceSupplyTest implements SingleClassUnitTest<ResourceSupply> {
 
@@ -27,5 +35,88 @@ public final class ResourceSupplyTest implements SingleClassUnitTest<ResourceSup
     @Test
     public void test_equals_negative() {
         // cannot be not equal
+    }
+
+    @Test
+    public void test_markResourceContribution_invalidArgument_noProfiles() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            this.createInstance().markResourceContribution(Collections.emptySet());
+        });
+    }
+
+    @Test
+    public void test_markResourceContribution_happyPath_noOptions() {
+        @Jailbreak ResourceProfile profile = ResourceProfileTest.createInstance();
+        profile.resourceItems.set(ResourceType.WOOD, 1);
+        @Jailbreak ResourceSupply supply = this.createSpyInstance();
+        when(supply.getPossibleSupplies()).thenReturn(Collections.emptySet());
+        assertEquals(Set.of(profile), supply.markResourceContribution(Set.of(profile)));
+    }
+
+    @Test
+    public void test_markResourceContribution_happyPath_singleOption() {
+        @Jailbreak ResourceProfile profile1 = ResourceProfileTest.createInstance();
+        profile1.resourceItems.set(ResourceType.WOOD, 1);
+        @Jailbreak ResourceProfile profile2 = ResourceProfileTest.createInstance();
+        profile2.resourceItems.set(ResourceType.WOOD, 2);
+        @Jailbreak ResourceProfile profile3 = ResourceProfileTest.createInstance();
+        profile3.resourceItems.set(ResourceType.WOOD, 3);
+        @Jailbreak ResourceSupply supply = this.createSpyInstance();
+        when(supply.getPossibleSupplies()).thenReturn(Set.of(ResourceType.WOOD));
+        assertEquals(Set.of(profile2, profile3), supply.markResourceContribution(Set.of(profile1, profile2)));
+    }
+
+    @Test
+    public void test_markResourceContribution_happyPath_multipleOptionsNoDuplicateResult() {
+        @Jailbreak ResourceProfile profile1 = ResourceProfileTest.createInstance();
+        profile1.resourceItems
+                .set(ResourceType.WOOD, 1)
+                .set(ResourceType.CLAY, 0);
+        @Jailbreak ResourceProfile profile2 = ResourceProfileTest.createInstance();
+        profile2.resourceItems
+                .set(ResourceType.WOOD, 2)
+                .set(ResourceType.CLAY, 0);
+        @Jailbreak ResourceProfile profile3 = ResourceProfileTest.createInstance();
+        profile3.resourceItems
+                .set(ResourceType.WOOD, 3)
+                .set(ResourceType.CLAY, 0);
+        @Jailbreak ResourceProfile profile4 = ResourceProfileTest.createInstance();
+        profile4.resourceItems
+                .set(ResourceType.WOOD, 1)
+                .set(ResourceType.CLAY, 1);
+        @Jailbreak ResourceProfile profile5 = ResourceProfileTest.createInstance();
+        profile5.resourceItems
+                .set(ResourceType.WOOD, 2)
+                .set(ResourceType.CLAY, 1);
+        @Jailbreak ResourceSupply supply = this.createSpyInstance();
+        when(supply.getPossibleSupplies()).thenReturn(Set.of(ResourceType.WOOD, ResourceType.CLAY));
+        assertEquals(Set.of(profile2, profile3, profile4, profile5), supply.markResourceContribution(Set.of(profile1, profile2)));
+    }
+
+    @Test
+    public void test_markResourceContribution_happyPath_multipleOptionsWithDuplicateResult() {
+        @Jailbreak ResourceProfile profile1 = ResourceProfileTest.createInstance();
+        profile1.resourceItems
+                .set(ResourceType.WOOD, 1)
+                .set(ResourceType.CLAY, 0);
+        @Jailbreak ResourceProfile profile2 = ResourceProfileTest.createInstance();
+        profile2.resourceItems
+                .set(ResourceType.WOOD, 0)
+                .set(ResourceType.CLAY, 1);
+        @Jailbreak ResourceProfile profile3 = ResourceProfileTest.createInstance();
+        profile3.resourceItems
+                .set(ResourceType.WOOD, 2)
+                .set(ResourceType.CLAY, 0);
+        @Jailbreak ResourceProfile profile4 = ResourceProfileTest.createInstance();
+        profile4.resourceItems
+                .set(ResourceType.WOOD, 1)
+                .set(ResourceType.CLAY, 1);
+        @Jailbreak ResourceProfile profile5 = ResourceProfileTest.createInstance();
+        profile5.resourceItems
+                .set(ResourceType.WOOD, 0)
+                .set(ResourceType.CLAY, 2);
+        @Jailbreak ResourceSupply supply = this.createSpyInstance();
+        when(supply.getPossibleSupplies()).thenReturn(Set.of(ResourceType.WOOD, ResourceType.CLAY));
+        assertEquals(Set.of(profile3, profile4, profile5), supply.markResourceContribution(Set.of(profile1, profile2)));
     }
 }
