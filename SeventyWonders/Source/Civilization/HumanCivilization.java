1f0038917b5e0035d01ec57b534c1ac3d31b516c
William Fu <williamfu4github@gmail.com>
2020-02-27 15:20:16 -0500

    add field developedAspects to class HumanCivilization

--- SeventyWonders/Source/Civilization/HumanCivilization.java
+++ SeventyWonders/Source/Civilization/HumanCivilization.java
@@ -1,10 +1,16 @@
 package Civilization;
 
+import Aspect.CivilizationAspect;
+import com.google.common.collect.Multiset;
 import lombok.EqualsAndHashCode;
 
 @EqualsAndHashCode
 public abstract class HumanCivilization {
 
+    @EqualsAndHashCode.Include
+    private Multiset<CivilizationAspect> developedAspects;
+
     protected HumanCivilization() {
+        this.developedAspects = Multiset.<CivilizationAspect>emptyMultiset();
     }
 }
--- SeventyWonders/Test/UnitTest/Civilization/HumanCivilizationTest.java
+++ SeventyWonders/Test/UnitTest/Civilization/HumanCivilizationTest.java
@@ -2,28 +2,55 @@ package UnitTest.Civilization;
 
 import Civilization.HumanCivilization;
 import TesterBase.UnitTest.SingleClassUnitTest;
+import com.google.common.collect.Multiset;
+import manifold.ext.api.Jailbreak;
 import org.junit.jupiter.api.Test;
+import static Utility.TestInstanceFactory.MineTest;
+import static Utility.TestInstanceFactory.OreVeinTest;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 public final class HumanCivilizationTest implements SingleClassUnitTest<HumanCivilization> {
 
+    private final class HumanCivilizationSubclass extends HumanCivilization {
+    }
+
     @Override
-    public void clearInstance(HumanCivilization instance) {
+    public void clearInstance(@Jailbreak HumanCivilization instance) {
+        instance.developedAspects.clear();
     }
 
     @Override
     public HumanCivilization createRealInstance() {
         // TODO return real object
-        return null;
+        return new HumanCivilizationSubclass();
     }
 
     @Test
     public void test_equals_positive() {
-        assertEquals(this.createInstance(), this.createInstance());
+        @Jailbreak HumanCivilization civilization1 = this.createInstance();
+        civilization1.developedAspects
+                .insert(MineTest.createInstance());
+        @Jailbreak HumanCivilization civilization2 = this.createInstance();
+        civilization2.developedAspects
+                .insert(MineTest.createInstance());
+        assertEquals(civilization1, civilization2);
     }
 
     @Test
     public void test_equals_negative() {
-        // cannot be not equal
+        @Jailbreak HumanCivilization civilization1 = this.createInstance();
+        civilization1.developedAspects
+                .insert(MineTest.createInstance());
+        @Jailbreak HumanCivilization civilization2 = this.createInstance();
+        civilization2.developedAspects
+                .insert(OreVeinTest.createInstance());
+        assertNotEquals(civilization1, civilization2);
+    }
+
+    @Test
+    public void test_constructor_happyPath() {
+        @Jailbreak HumanCivilization civilization = new HumanCivilizationSubclass();
+        assertEquals(Multiset.emptyMultiset(), civilization.developedAspects);
     }
 }
